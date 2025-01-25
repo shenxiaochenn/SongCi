2482a684a4338d663d8542de4b1b26c119ba6933
shen chen <90724430+shenxiaochenn@users.noreply.github.com>
2025-01-26 00:01:35 +0800

    Update datasets.py

--- cross-modal-decoder/datasets.py
+++ cross-modal-decoder/datasets.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/home/wangzhenyuan/CONCH/CONCH/conch/')
+#sys.path.append('/home/wangzhenyuan/CONCH/CONCH/conch/')
 import torch
 from torch.utils import data
 import numpy as np
@@ -8,7 +8,7 @@ import random
 import open_clip
 from transformers import CLIPProcessor, CLIPModel
 from transformers import AutoTokenizer
-from open_clip_custom import create_model_from_pretrained, get_tokenizer, tokenize
+#from open_clip_custom import create_model_from_pretrained, get_tokenizer, tokenize  #### FOR CONCH
 class Mydataset(data.Dataset):
     def __init__(self, root="./tidy_data.csv",randomm=True):
         self.data = pd.read_csv(root)
@@ -255,33 +255,33 @@ class Mydataset_clipp(data.Dataset):
         return len(self.data)
 
 
-class Mydataset_conch(data.Dataset):
-    def __init__(self, root="./tidy_data.csv",randomm=True):
-        self.data = pd.read_csv(root)
-        #self.tokenizer = open_clip.get_tokenizer('hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224')
-        self.tokenizer_2 = get_tokenizer()
-        self.img_length = 128
-        self.randomm = randomm
-    def __getitem__(self, index):
-        if self.randomm:
-            disease = random.choice(self.data.iloc[index,]["disease"].split("/"))
-        else:
-            disease = self.data.iloc[index,]["disease"].split("/")[0]
-        description = self.data.iloc[index,]["description"]
-        img = torch.tensor(list(eval(self.data.iloc[index,]["prototype"]).keys()))
-        img_num = torch.tensor(list(eval(self.data.iloc[index,]["prototype"]).values()))
-        img_num = torch.where(img_num > 10000, 10000, img_num)
-        tokens_disease = tokenize(texts=[disease], tokenizer=self.tokenizer_2)
-        tokens_description = tokenize(texts=[description], tokenizer=self.tokenizer_2)
-        if len(img)<self.img_length:
-            pad = self.img_length-len(img)
-            padding = torch.tensor(1024).repeat(pad)
-            padding_num = torch.tensor(0).repeat(pad)
-            img = torch.cat((img,padding))
-            img_num = torch.cat((img_num,padding_num))
-        if len(img)>self.img_length:
-            img = img[:self.img_length]
-            img_num = img_num[:self.img_length]
+# class Mydataset_conch(data.Dataset):
+#     def __init__(self, root="./tidy_data.csv",randomm=True):
+#         self.data = pd.read_csv(root)
+#         #self.tokenizer = open_clip.get_tokenizer('hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224')
+#         self.tokenizer_2 = get_tokenizer()
+#         self.img_length = 128
+#         self.randomm = randomm
+#     def __getitem__(self, index):
+#         if self.randomm:
+#             disease = random.choice(self.data.iloc[index,]["disease"].split("/"))
+#         else:
+#             disease = self.data.iloc[index,]["disease"].split("/")[0]
+#         description = self.data.iloc[index,]["description"]
+#         img = torch.tensor(list(eval(self.data.iloc[index,]["prototype"]).keys()))
+#         img_num = torch.tensor(list(eval(self.data.iloc[index,]["prototype"]).values()))
+#         img_num = torch.where(img_num > 10000, 10000, img_num)
+#         tokens_disease = tokenize(texts=[disease], tokenizer=self.tokenizer_2)
+#         tokens_description = tokenize(texts=[description], tokenizer=self.tokenizer_2)
+#         if len(img)<self.img_length:
+#             pad = self.img_length-len(img)
+#             padding = torch.tensor(1024).repeat(pad)
+#             padding_num = torch.tensor(0).repeat(pad)
+#             img = torch.cat((img,padding))
+#             img_num = torch.cat((img_num,padding_num))
+#         if len(img)>self.img_length:
+#             img = img[:self.img_length]
+#             img_num = img_num[:self.img_length]
 
         return tokens_disease,tokens_description,img,img_num
 
